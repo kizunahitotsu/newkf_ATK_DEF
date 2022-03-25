e319509612e09f70805e10a6db8054814aa26622
kizunahitotsu <79802083+kizunahitotsu@users.noreply.github.com>
2022-03-25 21:49:40 +0800

    Add files via upload

---
+++ iterate.py
@@ -0,0 +1,337 @@
+import os
+import re
+import time
+
+class GEAR:
+    def __init__(self,string):
+        pattern=r'(([A-Z]+) (\d+ \d+ \d+ \d+ \d+) (\d)|NONE)'
+        match=re.search(pattern,string).groups()
+        self.string=match[0]
+        self.type=match[1]
+        self.attribute=match[2]
+        if match[3]:
+            self.myst=bool(int(match[3]))
+        else:
+            self.myst=match[3]
+
+    def __str__(self):
+        return self.string
+    
+    def transform(self):
+        '''将神秘装备的名称导出'''
+        if self.myst:
+            return 'M_'+self.type
+        else:
+            return self.type
+
+class PC:
+    def __init__(self,string):
+        pattern=r'('+r'([A-Z]+)_((.+?)\|(.+?)\|turn(\d+)) (\d+ \d \d)\n'
+        pattern+=r'(?:WISH (\d \d \d \d \d \d \d)\n)?'
+        pattern+=r'(?:AMULET ((?:[A-Z]+ \d+ )+)ENDAMULET\n)?'
+        pattern+=r'(\d+ \d+ \d+ \d+ \d+ \d+) ?\n'
+        pattern+=r'([A-Z]+ \d+ \d+ \d+ \d+ \d+ \d|NONE)\n'*4
+        pattern+=r'(\d(?: [A-Z]+)*)'+r')'
+        match=re.search(pattern,string).groups()
+        self.string=match[0]
+        self.role=match[1]
+        self.name=match[2]
+        self.group=match[3]
+        self.mode=match[4]
+        self.turn=match[5]
+        self.card=match[6]
+        self.wish=match[7]
+        self.amulet=match[8]
+        self.attribute=match[9]
+        self.weapon=GEAR(match[10])
+        self.hand=GEAR(match[11])
+        self.body=GEAR(match[12])
+        self.head=GEAR(match[13])
+        self.aura=match[14]        
+
+    def __str__(self):
+        return self.string
+    
+    def __eq__(self,other):
+        return (self.role,self.name)==(other.role,other.name)
+    
+    def __hash__(self):
+        return hash((self.role,self.name))
+
+def read_lib():
+    '''读取lib.txt，返回包含信息的字典'''
+    with open('代码\\lib.txt',mode='r',encoding='UTF-8') as f:
+        lib=f.read()
+
+    pattern=r'\[(.+?)\]([^\[\]]+)'
+    match=re.findall(pattern,lib,flags=re.S)
+    dt=dict(map(lambda t:(t[0],tuple(t[1].split())),match))
+    return dt
+
+def read_option():
+    '''读取option.txt，返回包含信息的字典'''
+    with open('option.txt',mode='r',encoding='UTF-8') as f:
+        option=f.read()
+    
+    def read_info(info):
+        '''读取信息，以给定的形式返回'''
+        def read_info1(info):
+            '''读取形如a=x b=y的信息，返回包含信息的字典'''
+            return dict(re.findall(r'(.+)=(.+)',info))
+        
+        def read_info2(info):
+            '''读取形如a={} b={}的信息，返回包含信息的字典'''
+            match=re.findall(r'(.+)={(.+)}',info)
+            dt=dict(map(lambda t:(t[0],tuple(map(lambda s:tuple(s.split(' ')),t[1].split(';')))),match))
+            return dt
+
+        def read_info3(info):
+            '''读取形如{a=x b=y} {c=z d=w}的信息，返回包含信息的字典组成的元组'''
+            match=re.findall(r'{(.+?)}',info,flags=re.S)
+            dt=tuple(map(lambda t:dict(re.findall(r'(.+)=(.*)',t)),match))
+            return dt
+        
+        if(not re.search(r'[{}]',info)):
+            return read_info1(info)
+        elif(re.search(r'={.+}',info)):
+            return read_info2(info)
+        elif(re.search(r'{[^{}]*=[^{}]*}',info)):
+            return read_info3(info)
+
+    pattern=r'\[(.+?)\]([^\[\]]+)'
+    match=re.findall(pattern,option,flags=re.S)
+    dt=dict(map(lambda t:(t[0],read_info(t[1])),match)) #返回字典，value为option中key对应的信息
+    return dt
+
+def mode(defender):
+    '''将defender转化为mode'''
+    if(defender=='0'):
+        return 'ATK'
+    elif(defender=='1'):
+        return 'DEF'
+    elif(defender=='2'):
+        return 'MIX'
+
+def initialize(groups):
+    '''若首次进行迭代，则对'记录'文件夹进行初始化'''
+    try:
+        os.mkdir('记录')
+    except:
+        pass
+    
+    if not os.listdir('记录'):
+        with open('记录\\turn0.txt',mode='w+',encoding='UTF-8') as f:
+            for group in groups:
+                f.write(f'MIN_{group["Name"]}|{mode(group["Defender"])}|turn0 {group["Card"]}\n')
+                f.write('1 1 1 1 1 1\n')
+                f.write('NONE\n'*4)
+                f.write('0\n\n')
+
+def get_turn():
+    '''返回当前轮数'''
+    return max(map(lambda s:int(re.search(r'\d+',s).group()),os.listdir('记录')))
+
+def read_pool():
+    '''读取算点对手PC，返回字典，分别由总池子、ATK池子、DEF池子、MIX池子的列表构成'''
+    initialize(tuple(group for group in read_option()['Group']))
+    
+    pattern=r'('+r'[A-Z]+_.+?\|.+?\|turn\d+ \d+ \d \d\n'
+    pattern+=r'(WISH \d \d \d \d \d \d \d\n)?'
+    pattern+=r'(AMULET ([A-Z]+ \d+ )+ENDAMULET\n)?'
+    pattern+=r'\d+ \d+ \d+ \d+ \d+ \d+ ?\n'
+    pattern+=r'([A-Z]+ \d+ \d+ \d+ \d+ \d+ \d|NONE)\n'*4
+    pattern+=r'\d( [A-Z]+)*'+r')'
+    with open(f'记录\\turn{get_turn()}.txt',mode='r',encoding='UTF-8') as f:
+        pool_all=list(map(lambda s:PC(s.group()),re.finditer(pattern,f.read())))
+    
+    dt={'ALL':pool_all}
+    for m in ('ATK','DEF','MIX'):
+        dt[m]=[pc for pc in pool_all if pc.mode==m]
+    return dt
+
+def calculate():
+    '''调用newkf_64.exe进行算点，结果输出至output.txt'''
+    cmd='cd 代码 & newkf_64.exe < input.txt > output.txt'
+    os.system(cmd)
+
+def write_newkf_apc(group,role):
+    '''将当前group和当前role的PC信息写入newkf.in'''
+    with open('代码\\newkf_sample.in',mode='r',encoding='UTF-8') as f:
+        sample=f.read()
+    
+    sample=sample.replace('<Aura_value>',group['Aura value'],1)
+    sample=sample.replace('<Role>',role,1)
+    sample=sample.replace('<Card>',group['Card'],1)
+
+    if group['Wish']:
+        sample=sample.replace('<Wish>','WISH '+group['Wish'],1)
+    else:
+        sample=sample.replace('<Wish>\n','',1)
+
+    if group['Amulet']:
+        sample=sample.replace('<Amulet>','AMULET '+group['Amulet']+' ENDAMULET',1)
+    else:
+        sample=sample.replace('<Amulet>\n','',1)
+    
+    if group['Defender']=='0':
+        enemy=read_pool()['DEF']+read_pool()['MIX']
+    elif group['Defender']=='1':
+        enemy=read_pool()['ATK']+read_pool()['MIX']
+    elif group['Defender']=='2':
+        enemy=read_pool()['ALL']
+    sample=sample.replace('<Enemy>','\n\n'.join(map(lambda pc:pc.string,enemy)),1)
+
+    gear_string=''
+    if(group['Gear']):
+        for gear in read_lib()['Gear']:
+            if int(read_option()['Gear'][gear]) and gear not in read_option()['Role'][role][0]:
+                gear_string+=f'{gear} {group["Gear"]}\n'
+    if(group['Myst']):
+        for myst in read_lib()['Myst']:
+            if int(read_option()['Myst'][myst]) and myst not in read_option()['Role'][role][1]:
+                gear_string+=f'{myst} {group["Myst"]}\n'
+    sample=sample.replace('<Gear>\n',gear_string,1)
+
+    sample=sample.replace('<Threads>',read_option()['Calculation']['Threads'],1)
+    sample=sample.replace('<Tests>',read_option()['Calculation']['Tests'],1)
+    sample=sample.replace('<Seedmax>',read_option()['Calculation']['Seedmax'],1)
+
+    aura_string='_'.join(aura for aura in read_lib()['Aura']
+        if not int(read_option()['Aura'][aura]) or aura in read_option()['Role'][role][2])
+    if aura_string:
+        sample=sample.replace('<Aura_filter>',aura_string,1)
+    else:
+        sample=sample.replace('AURAFILTER <Aura_filter>\n','',1)
+
+    sample=sample.replace('<Defender>',group['Defender'],1)
+    sample=sample.replace('<Verbose>',read_option()['Calculation']['Verbose'],1)
+
+    with open('代码\\newkf.in',mode='w+',encoding='UTF-8') as f:
+        f.write(sample)
+
+def read_rezult_apc():
+    '''读取output.txt，返回PC算点结果和胜率的元组'''
+    with open('代码\\output.txt',mode='r',encoding='UTF-8') as f:
+        output=f.read()
+
+    pattern=r'[A-Z]+(_.+?\|.+?\|turn\d+)? \d+ \d \d\n'
+    pattern+=r'(WISH \d \d \d \d \d \d \d\n)?'
+    pattern+=r'(AMULET ([A-Z]+ \d+ )+ENDAMULET\n)?'
+    pattern+=r'\d+ \d+ \d+ \d+ \d+ \d+ ?\n'
+    pattern+=r'([A-Z]+ \d+ \d+ \d+ \d+ \d+ \d|NONE)\n'*4
+    pattern+=r'\d( [A-Z]+)*'
+    match=re.search(pattern,output)
+    pc_string=match.group()
+    
+    win_rate=float(re.search('Average Win Rate : ([\d\.]+)%',output).group(1))
+    return pc_string,win_rate
+
+def apc(group,role):
+    '''对当前group和当前role进行apc算点，返回胜率和PC算点结果的元组'''
+    with open('代码\\input.txt',mode='w+',encoding='UTF-8') as f:
+        f.write('apc\nq')
+
+    write_newkf_apc(group,role)
+    calculate()
+
+    return read_rezult_apc()
+
+def write_newkf_vb(pool_all):
+    '''将PC池子写入newkf.in'''
+    with open('代码\\newkf_sample.in',mode='r',encoding='UTF-8') as f:
+        sample=f.read()
+    
+    sample=sample.replace('<Aura_value>','0',1)
+    sample=sample.replace('<Role> <Card>\n<Wish>\n<Amulet>','LIN 700 4 8',1)
+    sample=sample.replace('<Enemy>','\n\n'.join(map(lambda pc:pc.string,pool_all)),1)
+    sample=sample.replace('<Gear>\n','',1)
+    sample=sample.replace('<Threads>',read_option()['Calculation']['Threads'],1)
+    sample=sample.replace('<Tests>',read_option()['Calculation']['Tests_vb'],1)
+    sample=sample.replace('<Seedmax>',read_option()['Calculation']['Seedmax'],1)
+    sample=sample.replace('AURAFILTER <Aura_filter>\n','',1)
+    sample=sample.replace('<Defender>','2',1)
+    sample=sample.replace('<Verbose>',read_option()['Calculation']['Verbose'],1)
+
+    with open('代码\\newkf.in',mode='w+',encoding='UTF-8') as f:
+        f.write(sample)
+
+def read_rezult_vb():
+    '''读取output.txt，返回胜率'''
+    with open('代码\\output.txt',mode='r',encoding='UTF-8') as f:
+        output=f.read()
+    
+    return float(re.search('Win Rate : ([\d\.]+)%',output).group(1))
+
+def vb(pool):
+    '''对pool内的PC两两之间进行vb算点，返回key为(pc_ATK,pc_DEF)，value为胜率的字典'''
+    write_newkf_vb(pool['ALL'])
+
+    dt={}
+    for pc_ATK in pool['ATK']+pool['MIX']:
+        for pc_DEF in pool['DEF']+pool['MIX']:
+            with open('代码\\input.txt',mode='w+',encoding='UTF-8') as f:
+                f.write(f'vb PC {pc_ATK.name} PC {pc_DEF.name}\nq')
+            
+            calculate()
+            dt[(pc_ATK,pc_DEF)]=read_rezult_vb()
+    
+    return dt
+
+def sum_win_rate(dt,pool,pc):
+    '''计算pc对pool中相应对手的胜率和，返回胜率和'''
+    if(pc.mode=='ATK'):
+        return sum(map(lambda e:dt[(pc,e)],pool['DEF']+pool['MIX']))
+    elif(pc.mode=='DEF'):
+        return sum(map(lambda e:(100-dt[(e,pc)]),pool['ATK']+pool['MIX']))
+    elif(pc.mode=='MIX'):
+        return sum(map(lambda e:dt[(pc,e)],pool['DEF']+pool['MIX']))+sum(map(lambda e:(100-dt[(e,pc)]),pool['ATK']+pool['MIX']))
+
+def iterate_group(group):
+    '''对当前group和所有role进行apc算点，返回最高胜率的PC'''
+    results=[apc(group,role) for role in read_lib()['Role']]
+    results.sort(key=lambda t:-t[1])
+    pc_string=results[0][0]
+    pattern=r'[A-Z]+(_.+?\|.+?\|turn\d+)? \d+ \d \d\n'
+    pattern+=r'(?:WISH \d \d \d \d \d \d \d\n)?'
+    pattern+=r'(?:AMULET (?:[A-Z]+ \d+ )+ENDAMULET\n)?'
+    pattern+=r'\d+ \d+ \d+ \d+ \d+ \d+ ?\n'
+    pattern+=r'(?:[A-Z]+ \d+ \d+ \d+ \d+ \d+ \d|NONE)\n'*4
+    pattern+=r'\d(?: [A-Z]+)*'
+    match=re.search(pattern,pc_string).group()
+    
+    return PC(match.replace(' ',f'_{group["Name"]}|{mode(group["Defender"])}|turn{get_turn()+1} ',1))
+
+def iterate_turn():
+    '''对所有group进行算点，将所得PC加入池子，两两之间进行vb算点，然后每组淘汰掉胜率最低的PC，所得池子写入txt'''
+    pool=read_pool()
+    for group in read_option()['Group']:
+        pc_new=iterate_group(group)
+        pool['ALL'].append(pc_new)
+        pool[pc_new.mode].append(pc_new)
+
+    dt=vb(pool)
+    with open(f'记录\\turn{get_turn()+1}.txt',mode='w+',encoding='UTF-8') as f:
+        for group in read_option()['Group']:
+            lt=[pc for pc in pool[mode(group['Defender'])] if pc.group==group['Name']]
+            lt.sort(key=lambda pc:-sum_win_rate(dt,pool,pc))
+            if(len(lt)>int(group['Size'])):
+                lt=lt[:int(group['Size'])]
+            for pc in lt:
+                f.write(pc.string+'\n\n')
+            del lt
+
+def main():
+    for i in range(int(read_option()['Iteration']['Turns'])):
+        start_time=time.time()
+        iterate_turn()
+        end_time=time.time()
+        print(f'已完成第 {get_turn()} 轮迭代，用时 {end_time-start_time} s！')
+
+try:
+    start_time_total=time.time()
+    main()
+    end_time_total=time.time()
+    print(f'Use time: {end_time-start_time} s')
+except Exception as e:
+    print(e)
+    input()
---
+++ statistics.py
@@ -0,0 +1,182 @@
+import os
+import re
+import time
+from openpyxl import Workbook
+
+#import iterate会出BUG，日后解决
+
+class GEAR:
+    def __init__(self,string):
+        pattern=r'(([A-Z]+) (\d+ \d+ \d+ \d+ \d+) (\d)|NONE)'
+        match=re.search(pattern,string).groups()
+        self.string=match[0]
+        self.type=match[1]
+        self.attribute=match[2]
+        if match[3]:
+            self.myst=bool(int(match[3]))
+        else:
+            self.myst=match[3]
+
+    def __str__(self):
+        return self.string
+    
+    def transform(self):
+        if self.myst:
+            return 'M_'+self.type
+        else:
+            return self.type
+
+class PC:
+    def __init__(self,string):
+        pattern=r'('+r'([A-Z]+)_((.+?)\|(.+?)\|turn(\d+)) (\d+ \d \d)\n'
+        pattern+=r'(?:WISH (\d \d \d \d \d \d \d)\n)?'
+        pattern+=r'(?:AMULET ((?:[A-Z]+ \d+ )+)ENDAMULET\n)?'
+        pattern+=r'(\d+ \d+ \d+ \d+ \d+ \d+) ?\n'
+        pattern+=r'([A-Z]+ \d+ \d+ \d+ \d+ \d+ \d|NONE)\n'*4
+        pattern+=r'(\d(?: [A-Z]+)*)'+r')'
+        match=re.search(pattern,string).groups()
+        self.string=match[0]
+        self.role=match[1]
+        self.name=match[2]
+        self.group=match[3]
+        self.mode=match[4]
+        self.turn=match[5]
+        self.card=match[6]
+        self.wish=match[7]
+        self.amulet=match[8]
+        self.attribute=match[9]
+        self.weapon=GEAR(match[10])
+        self.hand=GEAR(match[11])
+        self.body=GEAR(match[12])
+        self.head=GEAR(match[13])
+        self.aura=match[14]        
+
+    def __str__(self):
+        return self.string
+    
+    def __eq__(self,other):
+        return (self.role,self.name)==(other.role,other.name)
+    
+    def __hash__(self):
+        return hash((self.role,self.name))
+
+def read_option():
+    '''读取option.txt，返回包含信息的字典'''
+    with open('option.txt',mode='r',encoding='UTF-8') as f:
+        option=f.read()
+    
+    def read_info(info):
+        '''读取信息，以给定的形式返回'''
+        def read_info1(info):
+            '''读取形如a=x b=y的信息，返回包含信息的字典'''
+            return dict(re.findall(r'(.+)=(.+)',info))
+        
+        def read_info2(info):
+            '''读取形如a={} b={}的信息，返回包含信息的字典'''
+            match=re.findall(r'(.+)={(.+)}',info)
+            dt=dict(map(lambda t:(t[0],tuple(map(lambda s:tuple(s.split(' ')),t[1].split(';')))),match))
+            return dt
+
+        def read_info3(info):
+            '''读取形如{a=x b=y} {c=z d=w}的信息，返回包含信息的字典组成的元组'''
+            match=re.findall(r'{(.+?)}',info,flags=re.S)
+            dt=tuple(map(lambda t:dict(re.findall(r'(.+)=(.*)',t)),match))
+            return dt
+        
+        if(not re.search(r'[{}]',info)):
+            return read_info1(info)
+        elif(re.search(r'={.+}',info)):
+            return read_info2(info)
+        elif(re.search(r'{[^{}]*=[^{}]*}',info)):
+            return read_info3(info)
+
+    pattern=r'\[(.+?)\]([^\[\]]+)'
+    match=re.findall(pattern,option,flags=re.S)
+    dt=dict(map(lambda t:(t[0],read_info(t[1])),match)) #返回字典，value为option中key对应的信息
+    return dt
+
+def read_pools():
+    '''用户输入start和end，读取start至end的所有轮数的PC，返回key为pc、value为份额的字典'''
+    dir_name=input('请输入需要统计的文件夹名（留空则默认为\'\\记录\'）：')
+    if not dir_name:
+        dir_name='记录'
+
+    while(True):
+        start,end=tuple(map(int,input('请输入需统计的起始轮数(start,end)：').split()))
+        if(f'turn{end}.txt' in os.listdir(dir_name)):
+            break
+        else:
+            print(f'{dir_name}\\turn{end}.txt 不存在，请重新输入！')
+
+    pattern=r'[A-Z]+_.+?\|.+?\|turn\d+ \d+ \d \d\n'
+    pattern+=r'(WISH \d \d \d \d \d \d \d\n)?'
+    pattern+=r'(AMULET ([A-Z]+ \d+ )+ENDAMULET\n)?'
+    pattern+=r'\d+ \d+ \d+ \d+ \d+ \d+ ?\n'
+    pattern+=r'([A-Z]+ \d+ \d+ \d+ \d+ \d+ \d|NONE)\n'*4
+    pattern+=r'\d( [A-Z]+)*'
+    dt={}
+    
+    for turn in range(start,end+1):
+        with open(f'{dir_name}\\turn{turn}.txt',mode='r',encoding='UTF-8') as f:
+            pool_all=map(lambda s:PC(s.group()),re.finditer(pattern,f.read()))
+            for pc in pool_all:
+                if pc in dt:
+                    dt[pc]+=1
+                else:
+                    dt[pc]=1
+    
+    return dt
+
+def to_workbook(dt):
+    '''将dt的内容导出至指定名称的Excel文档'''
+    def mode(defender):
+        '''将defender转化为mode'''
+        if(defender=='0'):
+            return 'ATK'
+        elif(defender=='1'):
+            return 'DEF'
+        elif(defender=='2'):
+            return 'MIX'
+  
+    def row_all(pc):
+        return [pc.name,pc.group,pc.mode]+row_group(pc)
+    
+    def row_group(pc):
+        row=[int(pc.turn),dt[pc],pc.role]
+        row+=list(map(int,pc.attribute.split()))
+        row+=[pc.weapon.transform(),pc.hand.transform(),pc.body.transform(),pc.head.transform(),pc.aura]
+        row+=pc.aura.split()[1:]
+        return row
+
+    title_all=['名称','组名','攻守','轮数','份额','角色','力量','敏捷','智力','体魄','精神','意志',
+                '武器','手部','身体','头部','光环','光环1','光环2','光环3','光环4']
+    title_group=['轮数','份额','角色','力量','敏捷','智力','体魄','精神','意志',
+                '武器','手部','身体','头部','光环','光环1','光环2','光环3','光环4']
+
+    wb=Workbook()
+
+    ws=wb.create_sheet('原始数据')
+    ws.append(title_all)
+    for group in read_option()['Group']:
+        ws.temp=wb.create_sheet(group['Name']+'|'+mode(group['Defender']))
+        ws.temp.append(title_group)
+    for pc in dt:
+        ws.append(row_all(pc))
+        wb[pc.group+'|'+pc.mode].append(row_group(pc))
+
+    xlsx_name=input('请输入导出的文件名（留空则默认为\'统计结果.xlsx\'）：')
+    if not xlsx_name:
+        xlsx_name='统计结果'
+    wb.save(xlsx_name+'.xlsx')
+
+def main():
+    to_workbook(read_pools())
+
+try:
+    start_time=time.time()
+    main()
+    end_time=time.time()
+    print(f'Use time: {end_time-start_time} s')
+except Exception as e:
+    print(e)
+    input()
